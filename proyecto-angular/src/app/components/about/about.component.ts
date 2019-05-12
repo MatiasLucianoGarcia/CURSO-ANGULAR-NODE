76e64def4de29fb8d50ab55adcc818a645be39a5
Matias <mati.luciano.garcia@gmail.com>
2019-05-12 14:56:16 -0300

    Primer Com,it

---
+++ proyecto-angular/src/app/components/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
+  constructor() {
+    this.title="Matias Garcia";
+    this.subtitle="Desarrollador web";
+    this.web="prueba@prueba.com";
+  }
+
+  ngOnInit() {
+  }
+
+}
