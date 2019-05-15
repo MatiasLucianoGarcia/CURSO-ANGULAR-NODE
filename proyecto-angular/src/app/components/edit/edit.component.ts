6ffbb256bed10b597cfacea9d5af79c72a9b09f7
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-05-14 23:51:10 -0300

    Agregado Editar en cada proyecto

--- proyecto-angular/src/app/components/edit/edit.component.ts
+++ proyecto-angular/src/app/components/edit/edit.component.ts
@@ -3,35 +3,54 @@ import {Project} from '../../models/project';
 import {ProjectService} from '../../services/project.service';
 import {UploadService} from '../../services/upload.service';
 import {Global} from '../../services/global';
+import {Router,ActivatedRoute,Params} from '@angular/router'; 
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css'],
+  selector: 'app-edit',
+  templateUrl: '../create/create.component.html',
+  styleUrls: ['./edit.component.css'],
   providers: [ProjectService,UploadService]
 })
-export class CreateComponent implements OnInit {
-
+export class EditComponent implements OnInit {
+  
   public title:String;
   public project: Project;
   public save_project;
   public status: String;
   public filesToUpload: Array<File>;
+  public url:String;
+  
   constructor(
     private _projectService: ProjectService,
-    private _uploadService: UploadService
+    private _uploadService: UploadService,
+    private _route : ActivatedRoute,
+    private _router :Router 
   ){
-    this.title="Crear Proyecto";
-    this.project= new Project('','','','',2019,'','');
+    this.title="Editar Proyecto";
+    this.url = Global.url;
   }
 
   ngOnInit() {
+    this._route.params.subscribe(params =>{
+      let id =params.id;
+
+      this.getProject(id);
+    });
   }
 
-  onSubmit(form){
-    //Guardar datos
-    this._projectService.saveProject(this.project).subscribe(
-      response =>{
+  getProject(id){
+    this._projectService.getProject(id).subscribe(
+    response=>{
+        this.project=response.project;
+    },
+    error=>{
+      console.log(<any> error);
+    });
+  }
+
+  onSubmit(){
+    this._projectService.updateProject(this.project).subscribe(
+      response=>{
         if(response.project){
           //Subir la imagen
           if(this.filesToUpload){
@@ -40,21 +59,19 @@ export class CreateComponent implements OnInit {
               this.save_project=result.project;
 
               this.status="success";
-              form.reset();
             });
-          }else{
-            this.save_project=response.project;
+        }else{
+          this.save_project=response.project;
 
-            this.status="success";
-            form.reset();
-          }  
+          this.status="success";
         }
-        else{
+      }
+      else{
           this.status="failed";
-        }
+      }
       },
-      error =>{
-        console.log(<any>error);
+      error=>{
+        console.log(<any> error);
       }
     );
   }
@@ -62,4 +79,5 @@ export class CreateComponent implements OnInit {
   fileChangeEvent(fileInput:any){
     this.filesToUpload=<Array<File>> fileInput.target.files;
   }
+
 }
