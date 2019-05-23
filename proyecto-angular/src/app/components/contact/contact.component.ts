96f003619d871f69bf90d65fb0c7ea7b7fa9fed8
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-05-22 21:34:10 -0300

    Se agrego la libreria jQuery como dependencia. Se agrego tambien el plugin bxSlider. Recordar que no se estan enviando los node modules a la hora de cargar el proyecto en git.

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +13,18 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $("#logo").click(function(e){
+      $("header").css("background","green")
+                 .css("height","50px");
+    });
+
+    $(".galeria").bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 800,
+      speed: 500,
+      auto:true
+    });
   }
 
 }
